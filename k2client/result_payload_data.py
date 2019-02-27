46cf39d239e6736fc59e75b4b7967740d0383cf6
ThePhilosopherCodes <philip.wafula@kopokopo.com>
2019-02-27 13:48:02 +0300

    cleaned up decomposers

--- k2client/result_payload_data.py
+++ k2client/result_payload_data.py
@@ -1,6 +1,7 @@
 """Handles breaking down transaction payloads into their constituent elements of data """
-# from .setters.k2_result import Result
-from k2client.setters.k2_result import Result
+
+from k2client.setters import k2_result
+
 # define result type space
 buygoods_transaction_received = 'buygoods_transaction_received'
 buygoods_transaction_reversed = 'buygoods_transaction_reversed'
@@ -8,18 +9,19 @@ settlement_transfer_completed = 'settlement_transfer_completed'
 customer_created = 'customer_created'
 recieve_payments = 'payment_request'
 # revisit for topic
-create_payment = ''
+create_payment = 'create_payment'
 
 
-class ResultPayload(Result):
-    def __init__(self, result_payload):
-        self.decomposer = super().__init__()
-        self.result_payload = result_payload
+class ResultPayload(object):
+    def __init__(self, json_payload):
+        self.json_payload = json_payload
 
-    # function to breakdown transaction payloads
-    def decompose_result(self):
+    def decompose(self):
+        # define decomposer
+        decomposer = k2_result.Result()
+
         # define result type
-        result_type = self.result_payload['topic']
+        result_type = self.json_payload['topic']
 
         # access resource nest
         resource_payload_nest = self.result_payload['event']['resource']
@@ -33,91 +35,67 @@ class ResultPayload(Result):
         # access links nest
         links_payload_nest = self.result_payload['_links']
 
-        # check transaction payload type
+        # decompose all values that are similar in webhooks
         if result_type is buygoods_transaction_received or buygoods_transaction_reversed or recieve_payments:
-            # decompose webhook generic values
-            self.decomposer.first_name = resource_payload_nest['sender_first_name']
-            self.decomposer.middle_name = resource_payload_nest['sender_middle_name']
-            self.decomposer.last_name = resource_payload_nest['sender_last_name']
-            self.decomposer.amount = resource_payload_nest['amount']
-            self.decomposer.currency = resource_payload_nest['currency']
-            self.decomposer.system = resource_payload_nest['system']
-            self.decomposer.status = resource_payload_nest['status']
-            self.decomposer.reference = resource_payload_nest['reference']
-            self.decomposer.orgn_time = resource_payload_nest['origination_time']
 
+            decomposer.first_name = resource_payload_nest['sender_first_name']
+            decomposer.middle_name = resource_payload_nest['sender_middle_name']
+            decomposer.last_name = resource_payload_nest['sender_last_name']
+            decomposer.amount = resource_payload_nest['amount']
+            decomposer.currency = resource_payload_nest['currency']
+            decomposer.system = resource_payload_nest['system']
+            decomposer.status = resource_payload_nest['status']
+            decomposer.reference = resource_payload_nest['reference']
+            decomposer.orgn_time = resource_payload_nest['origination_time']
+
+        # decompose all values that have similar metadata structures
+        elif result_type is recieve_payments or create_payment:
+            decomposer.metadata_customer_id = metadata_payload_nest['customer_id']
+            decomposer.metadata_reference = metadata_payload_nest['reference']
+
+        # decompose all values that have similar links structures
+        elif result_type is buygoods_transaction_reversed \
+                or buygoods_transaction_received \
+                or settlement_transfer_completed \
+                or customer_created:
+            decomposer.links_self = links_payload_nest['self']
+            decomposer.links_resource = links_payload_nest['resource']
+
+        # decompose all values that are common to buygoods transactions
         elif result_type is buygoods_transaction_reversed or buygoods_transaction_received:
-            # decompose buygoods generic values
-            self.decomposer.till_number = resource_payload_nest['till_number']
-            self.decomposer.system = resource_payload_nest['system']
+            decomposer.till_number = resource_payload_nest['till_number']
+            decomposer.system = resource_payload_nest['system']
 
-            # decompose buygoods transaction reversed specific value
+            # decompose value specific to buygoods transaction reversed
             if result_type is buygoods_transaction_reversed:
-                self.decomposer.reversal_time = resource_payload_nest['reversal_time']
+                decomposer.reversal_time = resource_payload_nest['reversal_time']
 
+        # decompose all values that are common to recieve MPESA payments service
         elif result_type is recieve_payments:
             # recieve MPESA payment
-            self.decomposer.payment_result_id = self.result_payload['payment_request_id']
-            self.decomposer.payment_result_status = self.result_payload['status']
-
+            decomposer.payment_result_id = self.result_payload['payment_request_id']
+            decomposer.payment_result_status = self.result_payload['status']
             #  recieve MPESA payment errors
-            self.decomposer.error_code = error_payload_nest['code']
-            self.decomposer.error_description = error_payload_nest['description']
-
+            decomposer.error_code = error_payload_nest['code']
+            decomposer.error_description = error_payload_nest['description']
             # recieve MPESA payment metadata
-            self.decomposer.customer_id = metadata_payload_nest['customer_id']
-            self.decomposer.reference = metadata_payload_nest['reference']
-            self.decomposer.notes = metadata_payload_nest['notes']
-
+            decomposer.metadata_notes = metadata_payload_nest['notes']
             # recieve MPESA payment links
-            self.decomposer.payment_request = links_payload_nest['payment_request']
-            self.decomposer.links_resource = links_payload_nest['resource']
-            self.decomposer.links_self = links_payload_nest['self']
+            decomposer.payment_request = links_payload_nest['payment_request']
 
+        # decompose all values that are common to the PAY service
         elif result_type is create_payment:
-            # decompose PAY generic values
-            self.decomposer.amount = self.result_payload['amount']['value']
-            self.decomposer.currency = self.result_payload['amount']['currency']
-            self.decomposer.status = self.result_payload['status']
-            self.decomposer.reference = self.result_payload['reference']
-            self.decomposer.orgn_time = self.result_payload['origination_time']
-            self.decomposer.destination = self.result_payload['destination']
+            decomposer.amount = self.result_payload['amount']['value']
+            decomposer.currency = self.result_payload['amount']['currency']
+            decomposer.status = self.result_payload['status']
+            decomposer.reference = self.result_payload['reference']
+            decomposer.orgn_time = self.result_payload['origination_time']
+            decomposer.destination = self.result_payload['destination']
 
             # decompose PAY metadata
-            self.decomposer.customer_id = metadata_payload_nest['customer_id']
-            self.decomposer.notes = metadata_payload_nest['notes']
+            decomposer.notes = metadata_payload_nest['notes']
 
-            # decompose PAY links
-            self.decomposer.links_self = links_payload_nest['self']
-
-        return self.decomposer
+            return decomposer
 
 
-data = {
-    "id": "cac95329-9fa5-42f1-a4fc-c08af7b868fb",
-    "resourceId": "",
-    "topic": "payment_request",
-    "created_at": "2018-06-20T22:45:12.790Z",
-    "status": "Failed",
-    "event": {
-        "type": "Payment Request",
-        "resource": "",
-        "errors": [
-            {
-                "code": "501",
-                "description": "Insufficient funds"
-            }
-        ]
-    },
-    "metadata": {
-        "customer_id": "123456789",
-        "reference": "123456",
-        "notes": "Payment for invoice 123456"
-    },
-    "_links": {
-        "self": "https://api-sandbox.kopokopo.com/payment_request_results/cac95329-9fa5-42f1-a4fc-c08af7b868fb",
-        "payment_request": "https://api-sandbox.kopokopo.com/payment_requests/cac95329-9fa5-42f1-a4fc-c08af7b868fb"
-    }
-}
 
-print(ResultPayload(data).decompose_result().payment_request)
