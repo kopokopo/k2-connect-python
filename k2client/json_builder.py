d68ff19f328f0b291b8b96b1247be437828f24bb
ThePhilosopherCodes <philip.wafula@kopokopo.com>
2019-03-05 17:26:07 +0300

    Updated JSON builder functions

--- k2client/json_builder.py
+++ k2client/json_builder.py
@@ -1,50 +1,63 @@
 """Handles building json objects across the library """
-import requests
+
+
+# build metadata json object
+def metadata(metadata_dictionary):
+    metadata_object = metadata_dictionary
+    return metadata_object
+
+
+# build links json object
+def links(links_self=None,
+          links_call_back_url=None,
+          links_payment_request=None, ):
+    links_object = {
+        "self": links_self,
+        "call_back_url": links_call_back_url,
+        "payment_request": links_payment_request
+    }
+    return links_object
+
+
+# build amount json object
+def amount(currency, value):
+    amount_object = {
+        "currency": currency,
+        "value": value
+    }
+    return amount_object
+
+
+# build bank account pay recipient json object
+def bank_account(**kwargs):
+    bank_account_object = kwargs
+    return bank_account_object
+
 
 """Build JSON objects responsible for creating PAY recipients"""
 
 
-# build bank account pay recipient json object
-def bank_account_recipient(provided_name,
-                           provided_account_name,
-                           provided_bank_id,
-                           provided_bank_branch_id,
-                           provided_account_number,
-                           provided_email=None,
-                           provided_phone=None):
-    bank_account_recipient_object = {
-        "name": provided_name,
-        "account_name": provided_account_name,
-        "bank_id": provided_bank_id,
-        "bank_branch_id": provided_bank_branch_id,
-        "account_number": provided_account_number,
-        "email": provided_email,
-        "phone": provided_phone
-    }
-    return bank_account_recipient_object
-
-
 # build mobile wallet pay recipient json object
-def mobile_wallet_recipient(provided_first_name,
-                            provided_last_name,
-                            provided_phone,
-                            provided_network,
-                            provided_email=None):
-    mobile_wallet_recipient_object = {
-        "first_name": provided_first_name,
-        "last_name": provided_last_name,
-        "phone": provided_phone,
-        "network": provided_network,
-        "email": provided_email
+def mobile_wallet(first_name,
+                  last_name,
+                  phone,
+                  network,
+                  email=None):
+    mobile_wallet_object = {
+        "first_name": first_name,
+        "last_name": last_name,
+        "phone": phone,
+        "network": network,
+        "email": email
     }
-    return mobile_wallet_recipient_object
+    return mobile_wallet_object
 
 
 # build recipient json object for pay recipient to be created
-def pay_recipient(provided_recipient_type, provided_pay_recipient):
+def pay_recipient(recipient_type, recipient):
     recipient_object = {
-        "type": provided_recipient_type,
-        "pay_recipient": provided_pay_recipient
+        "type": recipient_type,
+        "pay_recipient": recipient
     }
     return recipient_object
 
@@ -53,70 +66,72 @@ def pay_recipient(provided_recipient_type, provided_pay_recipient):
 
 
 # build subscriber json object
-def subscriber(provided_first_name,
-               provided_last_name,
-               provided_phone,
-               provided_email):
+def subscriber(first_name,
+               last_name,
+               phone,
+               email=None):
     subscriber_object = {
-        "first_name": provided_first_name,
-        "last_name": provided_last_name,
-        "phone": provided_phone,
-        "email": provided_email
+        "first_name": first_name,
+        "last_name": last_name,
+        "phone": phone,
+        "email": email
     }
     return subscriber_object
 
 
-# build amount json object
-def amount(provided_currency, provided_value):
-    amount_object = {
-        "currency": provided_currency,
-        "value": provided_value
-    }
-    return amount_object
-
-
-# build metadata json object
-def metadata(**kwargs):
-    metadata_object = kwargs
-    return metadata_object
-
-
-# build links json object
-def links(provided_call_back_url):
-    links_object = {
-        "call_back_url": provided_call_back_url
-    }
-    return links_object
-
-
 # build payment request json object
-def payment_request(provided_payment_channel,
-                    provided_till_identifier,
-                    provided_subscriber,
-                    provided_amount,
-                    provided_links,
-                    provided_metadata=None):
+def stk_request(payment_channel,
+                till_identifier,
+                stk_subscriber,
+                stk_amount,
+                stk_links,
+                stk_metadata=None):
     payment_object = {
-        "payment_channel": provided_payment_channel,
-        "till_identifier": provided_till_identifier,
-        "subscriber": provided_subscriber,
-        "amount": provided_amount,
-        "metadata": provided_metadata,
-        "links": provided_links
+        "payment_channel": payment_channel,
+        "till_identifier": till_identifier,
+        "subscriber": stk_subscriber,
+        "amount": stk_amount,
+        "metadata": stk_metadata,
+        "links": stk_links
     }
     return payment_object
 
 
-"""Build JSON Objects for cretaing webhook subscriptions"""
+"""Build JSON Objects for creating webhook subscriptions"""
 
 
 # webhook json object builder
-def webhook_subscription(provided_event_type,
-                         provided_url,
-                         provided_webhook_secret):
+def webhook_subscription(event_type,
+                         url,
+                         webhook_secret):
     webhook_subscription_object = {
-        "event_type": provided_event_type,
-        "url": provided_url,
-        "webhook_secret": provided_webhook_secret
+        "event_type": event_type,
+        "url": url,
+        "webhook_secret": webhook_secret
     }
     return webhook_subscription_object
+
+
+""""Build JSON Objects for creating payments"""
+
+
+# build payment json object
+def payment(provided_destination,
+            provided_amount,
+            provided_metadata,
+            provided_links):
+    payment_json_object = {
+        "destination": provided_destination,
+        "amount": provided_amount,
+        "metadata": provided_metadata,
+        "_links": provided_links
+    }
+    return payment_json_object
+
+
+""""Build JSON Objects for creating payments"""
+
+
+def transfers(**kwargs):
+    transfer_json_object = kwargs
+    return transfer_json_object
