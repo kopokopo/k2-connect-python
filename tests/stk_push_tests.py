ec9a74ef896dcb5bf6cc6f29aa3f788485090cd1
ThePhilosopherCodes <philip.wafula@kopokopo.com>
2019-03-06 11:14:15 +0300

    Cleanup

--- tests/stk_push_tests.py
+++ tests/stk_push_tests.py
@@ -1,6 +1,10 @@
-def testing(**kwargs):
-    testing_vals = kwargs
-    return testing_vals
+import unittest
 
 
-print(testing(customer_id="customer_id"))
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(True, False)
+
+
+if __name__ == '__main__':
+    unittest.main()
