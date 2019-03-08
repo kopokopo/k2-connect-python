1e793df391a5859ec56d34da3840be4412b48821
ThePhilosopherCodes <philip.wafula@kopokopo.com>
2019-03-08 15:12:49 +0300

    Added 'attribute.py' to implement auto generation of setters and python properties

--- k2client/setters/receive_mpesa.py
+++ k2client/setters/receive_mpesa.py
@@ -1,7 +1,7 @@
-"""Handles creation of getters and setters for decomposition of result payloads"""
+"""Handles creation of properties and setters for ReceiveMPESAService"""
 
 
-class PaymentResult(object):
+class PaymentResult:
     def __init__(self,
                  payment_result_status=None,
                  payment_result_id=None):
@@ -14,18 +14,18 @@ class PaymentResult(object):
         self._payment_result_status = payment_result_status
         self._payment_result_id = payment_result_id
 
-    # payment_result_status
     @property
     def payment_result_status(self):
+        """Return string with payment result status """
         return self._payment_result_status
 
     @payment_result_status.setter
     def payment_result_status(self, value):
         self._payment_result_status = value
 
-    # payment_result_id
     @property
     def payment_result_id(self):
+        """Return string with payment result id """
         return self._payment_result_id
 
     @payment_result_id.setter
