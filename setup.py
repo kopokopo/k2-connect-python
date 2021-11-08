d33d035710eaeae77f44842a583724d7972daac5
DavidKar1uk1 <david.mwangi@kopokopo.com>
2021-11-08 12:37:06 +0300

    KK-5338: Update versioning.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as file_header:
 
 setuptools.setup(
     name='k2-connect',
-    version='1.2.1',
+    version='1.2.2',
     author='Philip Wafula, David Mwangi',
     author_email='philipwafula2@gmail.com, david.mwangi@kopokopo.com',
     description='A python SDK to connect to Kopo Kopo API',
